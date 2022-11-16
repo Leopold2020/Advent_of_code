eaebd24183e73bbbeddf62073a695039ed493ccf
Gossepojke <svedlund.oskar@gmail.com>
2022-11-16 15:35:52 +0100

    done with first, started with second.

---
+++ 2021/01system.py
@@ -0,0 +1,24 @@
+# Oskar Svedlund
+# TEINF-20
+# 2022-11-16
+# sonar
+
+previous_line = int()
+incresed = int()
+decresed = int()
+
+with open("2021/01input.txt", "r", encoding="utf-8") as f:
+    previous_line = int(f.readline())
+    for line in f.readlines():
+        
+        line = int(line)
+
+        if line > previous_line:
+            incresed += 1
+
+        if line < previous_line:
+            decresed += 1
+
+        previous_line = line
+
+print(incresed, decresed)
