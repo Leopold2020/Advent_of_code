796d3f9e2d2f04cdd22d667327e0f42bcaa619bf
Gossepojke <svedlund.oskar@gmail.com>
2022-11-23 12:13:27 +0100

    started day 1 of 2020

--- 2020/01system.py
+++ 2020/01system.py
@@ -1,6 +1,15 @@
 # Oskar Svedlund
 # Teinf-20
-# 2022-11-17
+# 2022-11-23
 # day 1 of 2020
 
+result = 0
+with open("2020/01input.txt", "r", encoding="utf-8") as reader:
+    for line1 in reader.readlines:
+        for line2 in reader.readlines:
 
+            if line1 + line2 == 2020:
+                result = line1 * line2 
+
+print(line1, line2)
+print(result)
