315e69ee523144a678efebc891d63e0b8faef4c7
Gossepojke <svedlund.oskar@gmail.com>
2022-12-02 13:32:38 +0100

    done with day 1 of 2022

--- 2022/day_1/part_2/01-2system2022.py
+++ 2022/day_1/part_2/01-2system2022.py
@@ -1,13 +1,13 @@
 # Oskar Svedlund
 # TEINF-20
-# 2022-12-01
-# Day 1 of 2022
+# 2022-12-02
+# day 1 part 2 of 2022
 
 elves = []
 values = []
 present_value = 0
 
-with open("2022/day_1/part_1/01-1input2022.txt", "r", encoding="utf-8") as reader:
+with open("2022/day_1/part_2/01-2input2022.txt", "r", encoding="utf-8") as reader:
     lines = reader.readlines()
 
     lines = [elem.replace("\n", "") for elem in lines]
@@ -25,4 +25,9 @@ with open("2022/day_1/part_1/01-1input2022.txt", "r", encoding="utf-8") as reade
             values.append(present_value)
             present_value = 0
 
-    print("largest number is ", max(values))
+values.sort()
+
+top_total = int(values[-1]) + int(values[-2]) + int(values[-3])
+
+
+print("largest three numbers combined is ", top_total)
