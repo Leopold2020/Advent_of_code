f2434bd27eac2a02ae7a0ff87b7fbbb68543f799
Gossepojke <svedlund.oskar@gmail.com>
2022-12-02 08:38:05 +0100

    started day 1 of 2022

---
+++ 2022/day_1/part_1/01-1system2022.py
@@ -0,0 +1,10 @@
+# Oskar Svedlund
+# TEINF-20
+# 2022-12-01
+# Day 1 of 2022
+
+
+
+with open("2022/day_1/part_1/01-1input2022.txt", "r", encoding="utf-8") as reader:
+    lines = reader.readlines()
+    
