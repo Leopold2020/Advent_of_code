377a6f72357ad18e12d5ed7df07fba9f2dc849cd
Gossepojke <svedlund.oskar@gmail.com>
2022-12-14 15:31:21 +0100

    day 4 of 2022 started

--- 2022/day_4/part_1/04-1system2022.py
+++ 2022/day_4/part_1/04-1system2022.py
@@ -5,3 +5,10 @@
 
 with open("2022/day_4/part_1/04-1input2022.txt", "r", encoding="utf-8") as reader:
     lines = reader.readlines
+    lines = [s.replace("\n", "") for s in lines]
+
+
+    for line in lines:
+
+        part_1, part_2 = line.split(",")
+
